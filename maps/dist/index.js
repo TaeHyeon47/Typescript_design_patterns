35ec00da17d105568e257c5c00e196ac1555a5e5
Tae Hyeon Jo <hillskuti@naver.com>
2022-11-02 23:07:27 +0900

    Duplicate Code

--- maps/dist/index.js
+++ maps/dist/index.js
@@ -193,7 +193,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56288" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61387" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
